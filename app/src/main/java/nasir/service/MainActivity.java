c91e3200aebd6ec8f75a9c89f0fcd6f3b71670b3
nazmul-nasir <nazmul.nasir@gmail.com>
2016-01-01 20:12:13 +0600

    Initial commit

---
+++ app/src/main/java/nasir/service/MainActivity.java
@@ -0,0 +1,28 @@
+package nasir.service;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.content.Intent;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    
+
+    // Method to start the service
+    public void startService(View view) {
+        startService(new Intent(getBaseContext(), MyService.class));
+    }
+
+    // Method to stop the service
+    public void stopService(View view) {
+        stopService(new Intent(getBaseContext(), MyService.class));
+    }
+}
---
+++ app/src/main/java/nasir/service/MyService.java
@@ -0,0 +1,30 @@
+package nasir.service;
+
+/**
+ * Created by Nasir on 1/1/2016.
+ */
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.widget.Toast;
+
+public class MyService extends Service {
+
+    @Override
+    public IBinder onBind(Intent arg0) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // Let it continue running until it is stopped.
+        Toast.makeText(this, "Alhamdulillah Service Started", Toast.LENGTH_LONG).show();
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "Alhamdulillah Service Destroyed", Toast.LENGTH_LONG).show();
+    }
+}
